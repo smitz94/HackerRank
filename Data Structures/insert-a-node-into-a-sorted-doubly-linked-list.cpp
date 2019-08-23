e7edae5f5fdbe4a2a714a21bb5c6d13f29807ffb
smit zaveri <smitzaveri12@gmail.com>
2019-08-23 14:37:55 +0530

    added solution

---
+++ Data Structures/insert-a-node-into-a-sorted-doubly-linked-list.cpp
@@ -0,0 +1,41 @@
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+
+    DoublyLinkedListNode *node= new DoublyLinkedListNode(data);
+
+    if(head==nullptr)
+        return node;
+
+    DoublyLinkedListNode *temp=head;
+
+    if(temp->data >=data)
+    {
+        node->next=temp;
+        node->next->prev=node;
+        return node;
+    }
+
+    while(temp->next!=nullptr)
+    {
+        if(data < temp->next->data){
+            
+            node->next=temp->next;
+            temp->next=node;
+            temp->next->prev=node;
+            node->prev=temp;
+            
+            return head;
+        }
+
+        temp=temp->next;
+    }
+
+    if(temp->next==nullptr && data>=temp->data)
+    {
+        temp->next=node;
+        node->prev=temp;
+    }
+
+return head;
+
+
+}
