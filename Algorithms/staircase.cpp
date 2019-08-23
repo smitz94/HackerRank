871f0c69e6e4658b32fe188a13aa6ee8da62558f
smit zaveri <smitzaveri12@gmail.com>
2019-08-23 08:43:27 +0530

    added solution

---
+++ Algorithms/staircase.cpp
@@ -0,0 +1,18 @@
+// Complete the staircase function below.
+void staircase(int n) {
+for(int i=0;i<n;i++)
+{
+    for(int j=0;j<n;j++)
+    {
+        if(j<n-i-1)
+        {
+            cout<<" ";
+        }
+        else
+            cout<<"#";
+    }
+
+    cout<<"\n";
+}
+
+}
