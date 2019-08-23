0db10c876bc5acbbf0541830ccbccf484a0fa53f
smit zaveri <smitzaveri12@gmail.com>
2019-08-23 09:40:07 +0530

    added solution

---
+++ Algorithms/making-anagrams.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string s1, s2;
+  int result;
+  int freq1[26] = {0}, freq2[26] = {0};
+
+  cin >> s1;
+  cin >> s2;
+
+  sort(s1.begin(), s1.end());
+  sort(s2.begin(), s2.end());
+
+  int count = 0, j = 0;
+  char counter = 'a';
+
+  for (int i = 0; i < 26; i++) {
+    count = 0;
+    while (counter == s1[j]) {
+      count++;
+      j++;
+    }
+    freq1[i] = count;
+    counter = counter + 1;
+  }
+
+  j = 0;
+  counter = 'a';
+  count = 0;
+
+  for (int i = 0; i < 26; i++) {
+    count = 0;
+    while (counter == s2[j]) {
+      count++;
+      j++;
+    }
+    freq2[i] = count;
+    counter = counter + 1;
+  }
+
+  result = 0;
+  for (int i = 0; i < 26; i++) {
+    result = result + abs(freq1[i] - freq2[i]);
+  }
+
+  cout << result;
+}
